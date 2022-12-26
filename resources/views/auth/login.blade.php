03e7323b620738b65cee0a2479a4c2d68b72b597
zrenv <hpujiono002@gmail.com>
2022-12-26 12:26:56 +0700

    menghapus forgot password dan remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,24 +44,7 @@
                                 </div>
                             @enderror
                           </div>
-                      </div>
-                      
-                        <!-- 2 column grid layout for inline styling -->
-                        <div class="row mb-4">
-                          <div class="col d-flex justify-content-center">
-                            <!-- Checkbox -->
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                              <label class="form-check-label" for="form2Example31"> Remember me </label>
-                            </div>
-                          </div>
-                      
-                          <div class="col">
-                            <!-- Simple link -->
-                            <a href="#!">Forgot password?</a>
-                          </div>
-                        </div>
-                      
+                      </div>                      
                         <!-- Submit button -->
                         <button type="submit" class="btn btn-primary btn-block mb-4 px-xl-5">Login</button>
                       
